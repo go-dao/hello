c0fa43fd7b68e42e54a85e5eba1ad11181d22e1d
wangzezhong <wangzezhong@zuoyebang.com>
2021-06-02 21:22:56 +0800

    evo

--- hello.go
+++ hello.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-    "fmt"
-    "github.com/go-dao/hello/morestrings"
+	"fmt"
+	"github.com/go-dao/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
-    fmt.Println(morestrings.ReverseRunes("Hello world."))
+	fmt.Println(morestrings.ReverseRunes("Hello world."))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
